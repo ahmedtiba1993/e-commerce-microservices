d84470c7046f4c531f9ff4a2c706eb792817f78d
Ahmed Tiba <ahmed.tiba.1993@gmail.com>
2024-06-19 21:14:11 +0100

    Create and configure the product micro service

---
+++ services/product/src/main/java/com/ahmed/ecommerce/exception/ProductPurchaseException.java
@@ -0,0 +1,8 @@
+package com.ahmed.ecommerce.exception;
+
+public class ProductPurchaseException extends RuntimeException{
+
+    public ProductPurchaseException(String message){
+        super(message);
+    }
+}
